3100ec8c0fac3a4a1c34cceb862bb0da58c2b6ad
nikeee13 <nikeee@outlook.com>
2014-05-23 00:36:25 +0200

    Add documentation

--- TeamSpeak3QueryApi/ParameterValueArray.cs
+++ TeamSpeak3QueryApi/ParameterValueArray.cs
@@ -2,18 +2,25 @@
 
 namespace TeamSpeak3QueryApi
 {
+    /// <summary>Represents the value of a parameter which consits of a multiple values.</summary>
     public class ParameterValueArray : IParameterValue
     {
         private readonly ParameterValue[] _sourceArray;
 
+        /// <summary>Creates a new instance of <see cref="T:TeamSpeak3QueryApi.ParameterValue"/> using <see langword="null" /> as a value.</summary>
         public ParameterValueArray()
             : this(null)
         { }
+
+        /// <summary>Creates a new instance of <see cref="T:TeamSpeak3QueryApi.ParameterValue"/> using the specified <see cref="T:System.String[]" /> as a value.</summary>
+        /// <param name="sourceArray">The values.</param>
         public ParameterValueArray(ParameterValue[] sourceArray)
         {
             _sourceArray = sourceArray;
         }
 
+        /// <summary>Creates an escaped string representation of the parameter value.</summary>
+        /// <returns>An escaped string representation of the parameter value.</returns>
         public string CreateParameterLine()
         {
             if (_sourceArray == null)
@@ -22,11 +29,15 @@ namespace TeamSpeak3QueryApi
             return string.Join("|", strs);
         }
 
+        /// <summary>Creates a new parameter value using a <see cref="T:System.String[]"/> as value.</summary>
+        /// <param name="fromParameters">The values</param>
         public static implicit operator ParameterValueArray(ParameterValue[] fromParameters)
         {
             return new ParameterValueArray(fromParameters);
         }
 
+        /// <summary>Returns a string that represents the current object.</summary>
+        /// <returns>A string that represents the current object.</returns>
         public override string ToString()
         {
             if (_sourceArray == null)
