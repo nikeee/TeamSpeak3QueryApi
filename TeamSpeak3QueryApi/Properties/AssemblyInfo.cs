ac22dc08f517b90ee6c7d87fd31477599501c666
nikeee13 <nikeee@outlook.com>
2014-05-23 01:25:35 +0200

    Update AssemblyInfo

--- TeamSpeak3QueryApi/Properties/AssemblyInfo.cs
+++ TeamSpeak3QueryApi/Properties/AssemblyInfo.cs
@@ -2,12 +2,12 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("CsTs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TeamSpeak3QueryApi")]
+[assembly: AssemblyDescription("Provides classes to access a remote host uisng the TeamSpeak 3 Query API.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("cs-ts")]
-[assembly: AssemblyCopyright("Copyright © Niklas Mollenhauer 2014")]
+[assembly: AssemblyProduct("TeamSpeak3QueryApi")]
+[assembly: AssemblyCopyright("Copyright © Niklas Mollenhauer <holzig@outlook.com> 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
