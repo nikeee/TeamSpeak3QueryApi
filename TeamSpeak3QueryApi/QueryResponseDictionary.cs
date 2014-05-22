f97502aa432e634e2484c0781927252be26a79b4
nikeee13 <nikeee@outlook.com>
2014-05-23 01:20:35 +0200

    Add documentation

--- TeamSpeak3QueryApi/QueryResponseDictionary.cs
+++ TeamSpeak3QueryApi/QueryResponseDictionary.cs
@@ -4,11 +4,15 @@ using System.Runtime.Serialization;
 
 namespace TeamSpeak3QueryApi
 {
+    /// <summary>Represents the data of a query response.</summary>
     [Serializable]
     public class QueryResponseDictionary : Dictionary<string, object>
     {
+        /// <summary>Creates a new instance of <see cref="QueryResponseDictionary"/>.</summary>
         public QueryResponseDictionary()
         { }
+
+        /// <summary>Initializes a new instance of the <see cref="QueryResponseDictionary"/> with serialized data.</summary>
         protected QueryResponseDictionary(SerializationInfo info, StreamingContext context)
             : base(info, context)
         { }
--- TeamSpeak3QueryApi/TeamSpeakQueryProtocolException.cs
+++ TeamSpeak3QueryApi/TeamSpeakQueryProtocolException.cs
@@ -2,6 +2,7 @@
 
 namespace TeamSpeak3QueryApi
 {
+    /// <summary>Represents errors that occur during queries caused by protcol violations.</summary>
     [Serializable]
     public class TeamSpeakQueryProtocolException : Exception
     { }
