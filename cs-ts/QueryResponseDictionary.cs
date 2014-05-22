28d438ff5ad49cfb9056f9b8d27b9bbf7339a36c
nikeee13 <nikeee@outlook.com>
2014-05-22 23:58:23 +0200

    Resolve code analysis issues

--- cs-ts/QueryResponseDictionary.cs
+++ cs-ts/QueryResponseDictionary.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace CsTs
 {
     [Serializable]
     public class QueryResponseDictionary : Dictionary<string, object>
     {
-
+        public QueryResponseDictionary()
+        { }
+        protected QueryResponseDictionary(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        { }
     }
 }
