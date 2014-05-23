920c7b48d333d6f160ad1b365fc4692cf3df17f1
nikeee13 <nikeee@outlook.com>
2014-05-23 11:03:13 +0200

    Add documentation

--- TeamSpeak3QueryApi/QueryException.cs
+++ TeamSpeak3QueryApi/QueryException.cs
@@ -11,26 +11,37 @@ namespace TeamSpeak3QueryApi.Net
         /// <returns>The returned error.</returns>
         public QueryError Error { get; private set; }
 
+        /// <summary>Initializes a new instance of the <see cref="T:TeamSpeak3QueryApi.Net.QueryException"/> class.</summary>
         public QueryException()
             : base()
         {
             Error = null;
         }
 
+        /// <summary>Initializes a new instance of the <see cref="T:TeamSpeak3QueryApi.Net.QueryException"/> class with a specified error message.</summary>
+        /// <param name="message">The message that describes the error.</param>
         public QueryException(string message)
             : this(message, null)
         { }
 
+        /// <summary>Initializes a new instance of the <see cref="T:TeamSpeak3QueryApi.Net.QueryException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.</summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (<see langword="langword">Nothing</see> in Visual Basic) if no inner exception is specified.</param>
         public QueryException(string message, Exception innerException)
             : base(message, innerException)
         { }
 
+        /// <summary>Initializes a new instance of the <see cref="T:TeamSpeak3QueryApi.Net.QueryException"/> class with a specified error returned by the Query API.</summary>
+        /// <param name="error">The <see cref="T:TeamSpeak3QueryApi.Net.QueryError"/> that was returned by the Query API.</param>
         public QueryException(QueryError error)
             : this("An error occurred during the query.")
         {
             Error = error;
         }
 
+        /// <summary>Initializes a new instance of the <see cref="T:TeamSpeak3QueryApi.Net.QueryException"/> class with serialized data.</summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual information about the source or destination.</param>
         protected QueryException(SerializationInfo info, StreamingContext context)
              : base(info, context)
         { }
--- TeamSpeak3QueryApi/TeamSpeakQueryProtocolException.cs
+++ TeamSpeak3QueryApi/TeamSpeakQueryProtocolException.cs
@@ -18,7 +18,7 @@ namespace TeamSpeak3QueryApi.Net
             : this(message, null)
         { }
 
-        /// <summary>Initializes a new instance of the <see cref="T:TeamSpeak3QueryApi.Net.TeamSpeakQueryProtocolException"/>class with a specified error message and a reference to the inner exception that is the cause of this exception.</summary>
+        /// <summary>Initializes a new instance of the <see cref="T:TeamSpeak3QueryApi.Net.TeamSpeakQueryProtocolException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.</summary>
         /// <param name="message">The error message that explains the reason for the exception.</param>
         /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (<see langword="langword">Nothing</see> in Visual Basic) if no inner exception is specified.</param>
         public TeamSpeakQueryProtocolException(string message, Exception innerException)
