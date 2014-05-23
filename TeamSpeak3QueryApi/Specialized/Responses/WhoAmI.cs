6129fbcdebe0563d44b3f0a5fcde651d5942a59e
nikeee13 <nikeee@outlook.com>
2014-05-23 23:47:59 +0200

    Implement WhoAmIResponse

--- TeamSpeak3QueryApi/Specialized/Responses/WhoAmI.cs
+++ TeamSpeak3QueryApi/Specialized/Responses/WhoAmI.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace TeamSpeak3QueryApi.Net.Specialized.Responses
+﻿namespace TeamSpeak3QueryApi.Net.Specialized.Responses
 {
     public class WhoAmI : Response
     {
-        // TODO
+        [QuerySerialize("virtualserver_status")]
+        public string VirtualServerStatus; // Status of the virtual server (online | virtual online | offline | booting up | shutting down| ...
+
+        [QuerySerialize("virtualserver_id")]
+        public int VirtualServerId;
+
+        [QuerySerialize("virtualserver_unique_identifier")]
+        public string VirtualServerUid;
+
+        [QuerySerialize("virtualserver_port")]
+        public short VirtualServerPort;
+
+        [QuerySerialize("client_id")]
+        public int ClientId;
+
+        [QuerySerialize("client_channel_id")]
+        public int ClientChannelId;
+
+        [QuerySerialize("client_nickname")]
+        public string ClientNickName;
+
+        [QuerySerialize("client_database_id")]
+        public int ClientDatabaseId;
+
+        [QuerySerialize("client_login_name")]
+        public string ClientLoginName;
+
+        [QuerySerialize("client_unique_identifier")]
+        public string ClientUid;
+
+        [QuerySerialize("client_origin_server_id")]
+        public int ClientOriginServerId;
     }
 }
