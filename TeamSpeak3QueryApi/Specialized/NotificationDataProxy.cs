4a1052d7ef0d4db15a58b92e84db81d25128801b
nikeee13 <nikeee@outlook.com>
2014-05-23 15:03:14 +0200

    Implementing serializing system

--- TeamSpeak3QueryApi/Specialized/NotificationDataProxy.cs
+++ TeamSpeak3QueryApi/Specialized/NotificationDataProxy.cs
@@ -7,31 +7,81 @@ using System.Reflection;
 
 namespace TeamSpeak3QueryApi.Net.Specialized
 {
-    class NotificationDataProxy
+    internal class NotificationDataProxy
     {
+        private static readonly ITypeCaster DefaultCaster = new StringTypeCaster();
+        private static readonly Dictionary<Type, ITypeCaster> Casters = new Dictionary<Type, ITypeCaster>
+                                                                {
+                                                                    {typeof(int), new Int32TypeCaster()},
+                                                                    {typeof(string), DefaultCaster},
+                                                                    {typeof(bool), new BooleanTypeCaster()}
+                                                                };
+
         public static IReadOnlyList<T> SerializeGeneric<T>(NotificationData data)
-            where T : Notify
+                where T : Notify
         {
             if (data.Payload.Count == 0)
                 return new ReadOnlyCollection<T>(new T[0]);
 
             var pl = data.Payload;
             var fields = typeof(T).GetFields(BindingFlags.Public);
-            var destType = Activator.CreateInstance<T>();
+
+            var destList = new List<T>(pl.Count);
 
             Debugger.Break();
 
             foreach (var item in pl)
             {
+                var destType = Activator.CreateInstance<T>();
                 foreach (var v in item)
                 {
                     var matchedEntry = fields.SingleOrDefault(fi => fi.CustomAttributes.OfType<QuerySerializeAttribute>().Any(qsa => qsa.Name == v.Key) || fi.Name == v.Key);
-
+                    if (matchedEntry != null)
+                    {
+                        if (Casters.ContainsKey(matchedEntry.FieldType))
+                        {
+                            var caster = Casters[matchedEntry.FieldType]; //_casters.Single(c => c.Key == matchedEntry.FieldType);
+                            matchedEntry.SetValue(destType, caster.Cast(v.Value));
+                        }
+                        else
+                        {
+                            matchedEntry.SetValue(destType, DefaultCaster.Cast(v.Value));
+                        }
+                    }
                 }
+                destList.Add(destType);
             }
 
+            return new ReadOnlyCollection<T>(destList);
+        }
+    }
 
-            throw new NotImplementedException();
+    interface ITypeCaster
+    {
+        object Cast(object source);
+    }
+
+    class Int32TypeCaster : ITypeCaster
+    {
+        public object Cast(object source)
+        {
+            return int.Parse((string)source);
+        }
+    }
+
+    class StringTypeCaster : ITypeCaster
+    {
+        public object Cast(object source)
+        {
+            return (string)source;
+        }
+    }
+
+    class BooleanTypeCaster : ITypeCaster
+    {
+        public object Cast(object source)
+        {
+            return int.Parse((string)source) != 0;
         }
     }
 }
