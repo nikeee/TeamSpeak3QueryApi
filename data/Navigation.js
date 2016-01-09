d0fdc930e9fc8aef6f322018b33731a290493ade
Niklas Mollenhauer <nikeee@outlook.com>
2016-01-09 03:53:55 +0100

    New docs

---
+++ data/Navigation.js
@@ -0,0 +1,362 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.navigationData = [
+			
+
+	{
+		id: 'TeamSpeak3QueryApi.Net',
+		text: 'TeamSpeak3QueryApi.Net',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/TeamSpeak3QueryApi.Net' },
+		children: [
+			
+ 
+
+		{
+			id: 'TeamSpeak3QueryApi.Net.Codec',
+			text: 'Codec',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Codec' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.CodecEncryptionMode',
+			text: 'CodecEncryptionMode',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.CodecEncryptionMode' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.HostBannerMode',
+			text: 'HostBannerMode',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.HostBannerMode' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.HostMessageMode',
+			text: 'HostMessageMode',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.HostMessageMode' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.IParameterValue',
+			text: 'IParameterValue',
+			icon: './assets/icons/interface_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.IParameterValue' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.LogLevel',
+			text: 'LogLevel',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.LogLevel' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.NotificationData',
+			text: 'NotificationData',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.NotificationData' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Parameter',
+			text: 'Parameter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Parameter' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.ParameterValue',
+			text: 'ParameterValue',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.ParameterValue' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.ParameterValueArray',
+			text: 'ParameterValueArray',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.ParameterValueArray' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.PermissionGroupDatabaseType',
+			text: 'PermissionGroupDatabaseType',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.PermissionGroupDatabaseType' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.PermissionGroupType',
+			text: 'PermissionGroupType',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.PermissionGroupType' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.QueryClient',
+			text: 'QueryClient',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.QueryClient' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.QueryError',
+			text: 'QueryError',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.QueryError' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.QueryException',
+			text: 'QueryException',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.QueryException' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.QueryProtocolException',
+			text: 'QueryProtocolException',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.QueryProtocolException' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.QueryResponseDictionary',
+			text: 'QueryResponseDictionary',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.QueryResponseDictionary' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.TokenType',
+			text: 'TokenType',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.TokenType' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'TeamSpeak3QueryApi.Net.Specialized',
+		text: 'TeamSpeak3QueryApi.Net.Specialized',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/TeamSpeak3QueryApi.Net.Specialized' },
+		children: [
+			
+ 
+
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.ClientType',
+			text: 'ClientType',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.ClientType' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.GetChannelOptions',
+			text: 'GetChannelOptions',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.GetChannelOptions' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.GetClientOptions',
+			text: 'GetClientOptions',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.GetClientOptions' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.GetServerOptions',
+			text: 'GetServerOptions',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.GetServerOptions' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.KickOrigin',
+			text: 'KickOrigin',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.KickOrigin' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.MessageTarget',
+			text: 'MessageTarget',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.MessageTarget' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.NotificationType',
+			text: 'NotificationType',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.NotificationType' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.ReasonId',
+			text: 'ReasonId',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.ReasonId' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.TeamSpeakClient',
+			text: 'TeamSpeakClient',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.TeamSpeakClient' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'TeamSpeak3QueryApi.Net.Specialized.Notifications',
+		text: 'TeamSpeak3QueryApi.Net.Specialized.Notifications',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/TeamSpeak3QueryApi.Net.Specialized.Notifications' },
+		children: [
+			
+ 
+
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Notifications.ChannelCreated',
+			text: 'ChannelCreated',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Notifications.ChannelCreated' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Notifications.ChannelDeleted',
+			text: 'ChannelDeleted',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Notifications.ChannelDeleted' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Notifications.ChannelDescriptionChanged',
+			text: 'ChannelDescriptionChanged',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Notifications.ChannelDescriptionChanged' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Notifications.ChannelEdited',
+			text: 'ChannelEdited',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Notifications.ChannelEdited' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Notifications.ChannelMoved',
+			text: 'ChannelMoved',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Notifications.ChannelMoved' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Notifications.ChannelPasswordChanged',
+			text: 'ChannelPasswordChanged',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Notifications.ChannelPasswordChanged' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Notifications.ClientEnterView',
+			text: 'ClientEnterView',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Notifications.ClientEnterView' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Notifications.ClientLeftView',
+			text: 'ClientLeftView',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Notifications.ClientLeftView' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Notifications.ClientMoved',
+			text: 'ClientMoved',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Notifications.ClientMoved' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Notifications.InvokerInformation',
+			text: 'InvokerInformation',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Notifications.InvokerInformation' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Notifications.Notification',
+			text: 'Notification',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Notifications.Notification' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Notifications.ServerEdited',
+			text: 'ServerEdited',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Notifications.ServerEdited' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Notifications.TextMessage',
+			text: 'TextMessage',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Notifications.TextMessage' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Notifications.TokenUsed',
+			text: 'TokenUsed',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Notifications.TokenUsed' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'TeamSpeak3QueryApi.Net.Specialized.Responses',
+		text: 'TeamSpeak3QueryApi.Net.Specialized.Responses',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/TeamSpeak3QueryApi.Net.Specialized.Responses' },
+		children: [
+			
+ 
+
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Responses.ClientBan',
+			text: 'ClientBan',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Responses.ClientBan' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Responses.CreatedChannel',
+			text: 'CreatedChannel',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Responses.CreatedChannel' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Responses.FoundChannel',
+			text: 'FoundChannel',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Responses.FoundChannel' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Responses.GetChannelInfo',
+			text: 'GetChannelInfo',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Responses.GetChannelInfo' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Responses.GetChannelListInfo',
+			text: 'GetChannelListInfo',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Responses.GetChannelListInfo' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Responses.GetClientsInfo',
+			text: 'GetClientsInfo',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Responses.GetClientsInfo' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Responses.GetServerListInfo',
+			text: 'GetServerListInfo',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Responses.GetServerListInfo' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Responses.Response',
+			text: 'Response',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Responses.Response' }
+        },	
+		{
+			id: 'TeamSpeak3QueryApi.Net.Specialized.Responses.WhoAmI',
+			text: 'WhoAmI',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/TeamSpeak3QueryApi.Net.Specialized.Responses.WhoAmI' }
+        },	
+
+
+		]
+	},
+
+
+	];
---
+++ data/Project.js
@@ -0,0 +1,11 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.projectData = {
+  name: 'TeamSpeak3QueryAPI',
+       author: 'Niklas Mollenhauer',         hasLogo: false,
+  targetFxs: [
+			".NET Framework 4.5",
+	  ]
+}
