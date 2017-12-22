5e4eec553df030a0ec78b24cc795cb732d3f57a5
Niklas Mollenhauer <nikeee@outlook.com>
2017-12-22 20:27:04 +0100

    Use async main

--- src/TeamSpeak3QueryApi.Demo/Program.cs
+++ src/TeamSpeak3QueryApi.Demo/Program.cs
@@ -10,14 +10,7 @@ namespace TeamSpeak3QueryApi.Net.Demo
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            DoItRich();
-            Console.WriteLine("Done");
-            Console.ReadLine();
-        }
-
-        static async void DoItRich()
+        static async void Main(string[] args)
         {
             var loginData = File.ReadAllLines("..\\..\\..\\logindata.secret");
 
@@ -40,15 +33,7 @@ namespace TeamSpeak3QueryApi.Net.Demo
             var currentClients = await rc.GetClients();
 
             var fullClients = currentClients.Where(c => c.Type == ClientType.FullClient).ToList();
-            //var fullClients = from c
-            //                  in currentClients
-            //                  where c.Type == ClientType.FullClient
-            //                  select c;
-            //fullClients.ForEach(async c=> await rc.KickClient(c, KickOrigin.Channel));
             await rc.KickClient(fullClients, KickOrigin.Channel);
-
-            //foreach (var client in fullClients)
-            //    await rc.KickClient(client.ClientId, KickTarget.Channel);
 
             // await rc.MoveClient(1, 1);
             // await rc.KickClient(1, KickTarget.Server);
@@ -59,39 +44,9 @@ namespace TeamSpeak3QueryApi.Net.Demo
             rc.Subscribe<ChannelEdited>(data => Debugger.Break());
             rc.Subscribe<ClientMoved>(data => Debugger.Break());
 
-            Console.WriteLine("Done1");
+            Console.WriteLine("Done");
+            Console.ReadLine();
         }
-
-        /*
-        static async void DoIt()
-        {
-            var loginData = File.ReadAllLines("..\\..\\..\\logindata.secret");
-
-            var host = loginData[0].Trim();
-            var user = loginData[1].Trim();
-            var password = loginData[2].Trim();
-
-            var cl = new QueryClient(host);
-
-            await cl.Connect();
-            await cl.Send("login", new Parameter("client_login_name", user), new Parameter("client_login_password", password));
-            await cl.Send("use", new Parameter("sid", 1));
-            await cl.Send("whoami");
-
-            //await cl.Send("servernotifyregister", new[] { "event", "channel" }, new[] { "id", "24" });
-            await cl.Send("servernotifyregister", new Parameter("event", "channel"), new Parameter("id", "24"));
-
-            cl.Subscribe("clientmoved", data =>
-                                        {
-                                            Console.WriteLine("Some client moved!");
-                                            cl.Unsubscribe("clientmoved");
-                                            cl.Send("servernotifyunregister", new[] { "event", "channel" }, new[] { "id", "24" });
-                                        });
-            // cl.Unsubscribe("message");
-
-            Console.WriteLine("Done1");
-        }
-        */
     }
 
     internal static class ReadOnlyCollectionExtensions
@@ -104,5 +59,4 @@ namespace TeamSpeak3QueryApi.Net.Demo
                 action(i);
         }
     }
-
 }
