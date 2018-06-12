6e4d5d4b084da9a4787d7b67de51d9fcbc25b569
Niklas Mollenhauer <nikeee@outlook.com>
2018-06-12 17:19:05 +0200

    Add missing source parameter

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 dotnet pack -c Release src/TeamSpeak3QueryApi/TeamSpeak3QueryApi.csproj
-dotnet nuget push -k $NUGET_KEY src/TeamSpeak3QueryApi/TeamSpeak3QueryApi/bin/Release/*.nuget
+dotnet nuget push -k $NUGET_KEY -s https://api.nuget.org/v3/index.json src/TeamSpeak3QueryApi/TeamSpeak3QueryApi/bin/Release/*.nuget
