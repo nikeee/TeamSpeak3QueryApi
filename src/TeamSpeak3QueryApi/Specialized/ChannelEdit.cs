b5d053a797c277eb24fcf334b94ce66811d3ca29
Aleksander Psuj <19539245+aleksanderpsuj@users.noreply.github.com>
2020-05-24 01:39:55 +0200

    Create ChannelEdit.cs

---
+++ src/TeamSpeak3QueryApi/Specialized/ChannelEdit.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace TeamSpeak3QueryApi.Net.Specialized
+{
+    [Flags]
+    public enum ChannelEdit
+    {
+        channel_name,
+        channel_topic,
+        channel_description,
+        channel_password,
+        channel,
+        codec,
+        channel_codec_quality,
+        channel_maxclients,
+        channel_maxfamilyclients,
+        channel_order,
+        channel_flag_permanent,
+        channel_flag_semi_permanent,
+        channel_flag_temporary,
+        channel_flag_default,
+        channel_flag_maxclients_unlimited,
+        channel_flag_maxfamilyclients_unlimited,
+        channel_flag_maxfamilyclients_inherited,
+        channel_needed_talk_power,
+        channel_name_phonetic,
+        channel_filepath,
+        channel_icon_id,
+        channel_codec_is_unencrypted,
+        cpid
+    }
+}
