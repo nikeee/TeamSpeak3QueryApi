02a74f9bd724a8b5fa6a2b2b44151c82d2aaa4bd
Niklas Mollenhauer <nikeee@outlook.com>
2021-12-03 10:27:12 +0100

    Use some new C# features to make demo more concise

--- src/TeamSpeak3QueryApi.Demo/Program.cs
+++ src/TeamSpeak3QueryApi.Demo/Program.cs
@@ -2,54 +2,45 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using TeamSpeak3QueryApi.Net.Specialized;
 using TeamSpeak3QueryApi.Net.Specialized.Notifications;
+using TeamSpeak3QueryApi.Net.Demo;
 
-namespace TeamSpeak3QueryApi.Net.Demo
-{
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            var loginData = File.ReadAllLines("logindata.secret");
+var loginData = File.ReadAllLines("logindata.secret");
 
-            var host = loginData[0].Trim();
-            var user = loginData[1].Trim();
-            var password = loginData[2].Trim();
+var host = loginData[0].Trim();
+var user = loginData[1].Trim();
+var password = loginData[2].Trim();
 
-            var rc = new TeamSpeakClient(host);
+using var rc = new TeamSpeakClient(host);
 
-            await rc.Connect();
+await rc.Connect();
 
-            await rc.Login(user, password);
-            await rc.UseServer(1);
+await rc.Login(user, password);
+await rc.UseServer(1);
 
-            await rc.WhoAmI();
+await rc.WhoAmI();
 
-            await rc.RegisterServerNotification();
-            await rc.RegisterChannelNotification(30);
+await rc.RegisterServerNotification();
+await rc.RegisterChannelNotification(30);
 
-            var serverGroups = await rc.GetServerGroups();
-            var firstNormalGroup = serverGroups?.FirstOrDefault(s => s.ServerGroupType == ServerGroupType.NormalGroup);
-            var groupClients = await rc.GetServerGroupClientList(firstNormalGroup.Id);
+var serverGroups = await rc.GetServerGroups();
+var firstNormalGroup = serverGroups?.FirstOrDefault(s => s.ServerGroupType == ServerGroupType.NormalGroup);
+var groupClients = await rc.GetServerGroupClientList(firstNormalGroup.Id);
 
-            var currentClients = await rc.GetClients();
+var currentClients = await rc.GetClients();
 
-            var fullClients = currentClients.Where(c => c.Type == ClientType.FullClient).ToList();
-            await rc.KickClient(fullClients, KickOrigin.Channel);
+var fullClients = currentClients.Where(c => c.Type == ClientType.FullClient).ToList();
+await rc.KickClient(fullClients, KickOrigin.Channel);
 
-            // await rc.MoveClient(1, 1);
-            // await rc.KickClient(1, KickTarget.Server);
+// await rc.MoveClient(1, 1);
+// await rc.KickClient(1, KickTarget.Server);
 
-            rc.Subscribe<ClientEnterView>(data => data.ForEach(c => Debug.WriteLine($"Client {c.NickName} joined.")));
-            rc.Subscribe<ClientLeftView>(data => data.ForEach(c => Debug.WriteLine($"Client with id {c.Id} left (kicked/banned/left).")));
-            rc.Subscribe<ServerEdited>(data => Debugger.Break());
-            rc.Subscribe<ChannelEdited>(data => Debugger.Break());
-            rc.Subscribe<ClientMoved>(data => Debugger.Break());
+rc.Subscribe<ClientEnterView>(data => data.ForEach(c => Debug.WriteLine($"Client {c.NickName} joined.")));
+rc.Subscribe<ClientLeftView>(data => data.ForEach(c => Debug.WriteLine($"Client with id {c.Id} left (kicked/banned/left).")));
+rc.Subscribe<ServerEdited>(data => Debugger.Break());
+rc.Subscribe<ChannelEdited>(data => Debugger.Break());
+rc.Subscribe<ClientMoved>(data => Debugger.Break());
 
-            Console.WriteLine("Done");
-            Console.ReadLine();
-        }
-    }
-}
+Console.WriteLine("Done");
+Console.ReadLine();
