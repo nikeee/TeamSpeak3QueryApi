57cfd0950cda23bc054ae0f5520c6d127cf6d773
Niklas Mollenhauer <nikeee@outlook.com>
2021-12-11 18:24:46 +0100

    Optimize imports

--- src/TeamSpeak3QueryApi/IProtocolClient.cs
+++ src/TeamSpeak3QueryApi/IProtocolClient.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Renci.SshNet;
-using Renci.SshNet.Common;
 
 namespace TeamSpeak3QueryApi.Net
 {
